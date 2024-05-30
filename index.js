47f63adf05f7064407f3ee4b7fc93bf6c30d2106
Piero Soares <piero@MacBook-Pro.local>
2024-05-29 23:09:22 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import inquirer from 'inquirer';
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+        "message": "Enter your url: ",
+        "name": "URL"
+    }
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+  })
+  .catch((error) => {
+        console.log("An error has happened.");
+  });
